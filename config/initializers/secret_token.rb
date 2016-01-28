4069c09ddcd43ef0076690f522ccf2ea9b05d352
Jeff <jeff.lobacz@gmail.com>
2016-01-28 01:43:03 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ApiOnly::Application.config.secret_token = 'cc9145ef777791ed5135a5e01a151e3b55ebbb84942ca41dac57f128c333f9f69aeb8b50ed71680ce5509bc66ec389d881908b629627a63a24bc04dc0cdaed29'
